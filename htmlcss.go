c891e36d3e10faa6f1b8856e295b7991cccd63f9
Will, Douglas A <Douglas_Will@cable.comcast.com>
2019-01-04 17:00:13 -0700

    Modified to not use the gorilla package

--- htmlcss.go
+++ htmlcss.go
@@ -1,14 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
-
-	"github.com/gorilla/mux"
 )
 
-var tmpls = template.Must(template.ParseFiles("templates/index.html"))
+//var tmpls = template.Must(template.ParseFiles("templates/index.html"))
+//var tmpls2 = template.Must(template.ParseFiles("templates/graphics.html"))
+
+var tmpls, _ = template.ParseFiles("templates/index.html", "templates/graphics.html")
 
 func Index(w http.ResponseWriter, r *http.Request) {
 	data := struct {
@@ -25,13 +27,34 @@ func Index(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func Graphics(w http.ResponseWriter, r *http.Request) {
+
+	data := struct {
+		Title  string
+		Header string
+		Slice  []string
+	}{
+		Title:  "Graphics Page",
+		Header: "Here are some Graphics!",
+	}
+
+	data.Slice = []string{"bob", "joe", "frank", "pete", "doug"}
+
+	fmt.Println("Serving Graphics")
+
+	if err := tmpls.ExecuteTemplate(w, "graphics.html", data); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
 func main() {
-	r := mux.NewRouter()
-	r.HandleFunc("/", Index)
 
-	r.PathPrefix("/styles/").Handler(http.StripPrefix("/styles/",
-		http.FileServer(http.Dir("templates/styles/"))))
+	server := http.Server{
+		Addr: "127.0.0.1:9000",
+	}
 
-	http.Handle("/", r)
-	log.Fatalln(http.ListenAndServe(":9000", nil))
+	http.HandleFunc("/", Index)
+	http.HandleFunc("/graphics", Graphics)
+	log.Fatalln(server.ListenAndServe())
 }
